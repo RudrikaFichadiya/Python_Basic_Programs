73873dee26d4b7f01b5826cb822c59787a086f37
RudrikaFichadiya <44025030+RudrikaFichadiya@users.noreply.github.com>
2018-10-16 17:47:00 +0530

    Delete Create_Database_Python_2.py

--- Python_with_SQLite_Database/Create_Database_Python_2.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-db = sqlite3.connect("Empolyees.sqlite")
-print("Opened database successfully...!!!")
-
-db.execute('''CREATE TABLE IF NOT EXISTS Employees 
-            (empid INTEGER PRIMARY KEY NOT NULL, name TEXT NOT NULL, age INTEGER, email TEXT, salary REAL)''')
-print("Database Created Successfully...!!!")
-db.commit()
-db.close()
